24a58d1a5eeed36fd968578208713f2b9d9c45b2
Émile <69686396+Kkiriya@users.noreply.github.com>
2025-12-11 16:16:57 -0500

    feat: skelette du code terminer

---
+++ Exercice 4 - Calculateur d'IMC avec Interface/historique.py
@@ -0,0 +1,18 @@
+# Author: Émile
+# Date: 2025-12-11
+# But: Sauvegarder le calcul imc et le nom de la personnne dans un fichier
+#      Lire le fichier et afficher son contenue
+
+def sauvegarder_calcul(nom, imc):
+    #Sauvegarde dans un fichier
+    pass
+
+def afficher_historiquqe():
+    #Lit le fichier
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
