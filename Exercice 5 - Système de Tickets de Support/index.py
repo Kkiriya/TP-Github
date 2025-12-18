5fe2f6f6b94bd227a717aa34d79ec6c6cd4a4617
Émile <69686396+Kkiriya@users.noreply.github.com>
2025-12-18 17:39:52 -0500

    test de conflit

--- Exercice 5 - Système de Tickets de Support/index.py
+++ Exercice 5 - Système de Tickets de Support/index.py
@@ -5,5 +5,5 @@
 print("Membre 1 : fichier gestion.py ")
 print("Membre 2: interface.py")
 print("Membre 3: sauvegarde.py")
-print("Membre 4: ticket.py")
+print("Membre 4: Test de conflit")
 print("Membre 5: main.py")
