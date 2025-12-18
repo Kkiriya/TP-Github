3954706c13360578089ed7791c11babf08938dc6
ammour nadjib <nadjib.ammour@gmail.com>
2025-12-18 17:35:51 -0500

    Ajout fichier index pour les tests

---
+++ Exercice 5 - Système de Tickets de Support/index.py
@@ -0,0 +1,9 @@
+#Instruction pour l'exercice 5
+#Simulation de conflit
+#Modifier la meme ligne par les membres
+
+print("Membre 1 : fichier gestion.py ")
+print("Membre 2: interface.py")
+print("Membre 3: sauvegarde.py")
+print("Membre 4: ticket.py")
+print("Membre 5: main.py")
