4845bf517b2373fcb22a3b7acf158c8826f14b6a
ammour nadjib <nadjib.ammour@gmail.com>
2025-12-20 17:10:33 -0500

    fix: correction interface et orchestration main IMC

--- Exercice 4 - Calculateur d'IMC avec Interface/main.py
+++ Exercice 4 - Calculateur d'IMC avec Interface/main.py
@@ -1,5 +1,26 @@
-# Date: 2025-12-08
-# Auteur: Émile V
-# But: Émile, Nadjib, Neil, Bruno
+#Main.py (nadjib)
 
-# Orchestre tout + tests unitaires
+import calculs
+import interface
+from historique import Historique
+
+
+def main():
+    # 1. Demande des infos utilisateur
+    nom, poids, taille = interface.demander_donnees()
+
+    # 2. Calcul IMC
+    imc = calculs.calculer_imc(poids, taille)
+    categorie = calculs.interpreter_imc(imc)
+
+    # 3. Affichage du résultat
+    interface.afficher_resultat(imc, categorie)
+
+    # 4. Sauvegarde dans l'historique
+    h = Historique(nom, imc)
+    message = h.sauvegarder_calcul()
+    print(message)
+
+
+if __name__ == "__main__":
+    main()
